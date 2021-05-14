95d2ee594b71112d9d3708a0f309325d2b4e0867
BuildTools <unconfigured@null.spigotmc.org>
2021-05-13 19:57:03 -0500

    Update publish script to build Release files instead of debug

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-dotnet.exe publish -r win-x86
-dotnet.exe publish -r win-x64
-dotnet.exe publish -r linux-x64
-dotnet.exe publish -r osx-x64
+dotnet.exe publish -c Release -r win-x86
+dotnet.exe publish -c Release -r win-x64
+dotnet.exe publish -c Release -r linux-x64
+dotnet.exe publish -c Release -r osx-x64
